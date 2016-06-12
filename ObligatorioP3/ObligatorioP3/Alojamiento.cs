0497d80db89c9bc12755cdfc9b30c0b930de609e
Sebastian Flaquer <sebastianflaquer@hotmail.com>
2016-06-12 17:11:17 -0300

    Ajustes

--- ObligatorioP3/ObligatorioP3/Alojamiento.cs
+++ ObligatorioP3/ObligatorioP3/Alojamiento.cs
@@ -13,11 +13,11 @@ namespace BienvenidosUY
     {
         public int id { get; set; }
         public string nombre { get; set; }
-        public Categoria categoria { get; set; }
+        public virtual Categoria categoria { get; set; }
         public string tipoHabitacion { get; set; }
         public bool banioPrivado { get; set; }
         public int cantHuespedes { get; set; }
-        public Ciudad ciudad { get; set; }
+        public virtual Ciudad ciudad { get; set; }
         public string barrio { get; set; }
         public List<Servicio> servicios {get; set;}
         public int calificacion { get; set; }
--- ObligatorioP3/ObligatorioP3/Anuncio.cs
+++ ObligatorioP3/ObligatorioP3/Anuncio.cs
@@ -13,7 +13,7 @@ namespace BienvenidosUY
     {
         public int id { get; set; }
         public string nombre { get; set; }
-        public Alojamiento alojamiento { get; set; }
+        public virtual Alojamiento alojamiento { get; set; }
         public string descripcion { get; set; }
         public string direccion1 { get; set; }
         public string direccion2 { get; set; }
