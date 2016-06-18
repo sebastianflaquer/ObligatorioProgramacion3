33cbb01cee412e763145e7e1cf1f024b0725204a
Sebastian Flaquer <sebastianflaquer@hotmail.com>
2016-06-18 16:57:00 -0300

    ajustes

--- ObligatorioP3/MVC/Models/Reserva.cs
+++ ObligatorioP3/MVC/Models/Reserva.cs
@@ -3,16 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using BienvenidosUY;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MVC.Models
 {
     public class Reserva
     {
         public int Id { get; set; }
+
         public DateTime FechaInicio { get; set; }
+
         public DateTime FechaFin { get; set; }
+
         public Registrado Registrado { get; set; }
+
+        [Range (1,10)]
+        [DataType(DataType.Currency)]
         public int CantHuespedes { get; set; }
+
+        [Required]
+        [StringLength(250)]
         public string TextoConsultas { get; set; }
 
     }
